68852cf15a40c3b167672c72cad39d1683cfcc65
sunflower <anzixuan168@163.com>
2024-11-18 22:06:08 +0800

    feat: Add sum and sort

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 export { default as isPrime} from './src/algorithms/isPrime/index';
+export { default as sort} from './src/algorithms/sort/index';
+export { default as sum} from './src/algorithms/sum/index';
