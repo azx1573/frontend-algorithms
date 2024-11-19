7375be0d7cc5ad64c803785a35e07c9371c959e7
sunflower <anzixuan168@163.com>
2024-11-19 09:08:46 +0800

    feat: Add removeSameElement

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as binarytree} from './src/algorithms/binarytree/index';
 export { default as isPrime} from './src/algorithms/isPrime/index';
+export { default as removeSame} from './src/algorithms/removeSame/index';
 export { default as sort} from './src/algorithms/sort/index';
 export { default as sum} from './src/algorithms/sum/index';
