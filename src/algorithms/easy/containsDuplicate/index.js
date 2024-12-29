ef04a67d8adc992115bc4745ee65ebc690b6cfe2
sunflower <anzixuan168@163.com>
2024-12-29 13:13:43 +0800

    docs: add some dosc files

--- src/algorithms/easy/containsDuplicate/index.js
+++ src/algorithms/easy/containsDuplicate/index.js
@@ -4,29 +4,27 @@
  * @returns
  */
 function containsDuplicate(nums) {
-  const set = new Set();
-  for (const num of nums) {
-    if (set.has(num)) {
-      return true;
+    var set = new Set();
+    for (var _i = 0, nums_1 = nums; _i < nums_1.length; _i++) {
+        var num = nums_1[_i];
+        if (set.has(num)) {
+            return true;
+        }
+        set.add(num);
     }
-    set.add(num);
-  }
-  return false;
+    return false;
 }
-
 /**
  * 方法二：去重后判断数组长度是否相等
  * @param nums
  */
 function containsDuplicate2(nums) {
-  return new Set(nums).size !== nums.length;
+    return new Set(nums).size !== nums.length;
 }
-
 // test cases
 console.log(containsDuplicate([1, 2, 3, 1])); // true
 console.log(containsDuplicate([1, 2, 3, 4])); // false
 console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
-
 console.log(containsDuplicate2([1, 2, 3, 1])); // true
 console.log(containsDuplicate2([1, 2, 3, 4])); // false
 console.log(containsDuplicate2([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
