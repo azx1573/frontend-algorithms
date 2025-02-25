925bd8b399ba19207027abe2a8b79beea1bb1e0b
sunflower <anzixuan168@163.com>
2025-02-26 00:25:01 +0800

    feat: optimize  findPrimeFactor

--- src/algorithms/easy/findPrimeFactor/index.js
+++ src/algorithms/easy/findPrimeFactor/index.js
@@ -7,19 +7,25 @@ function findPrimeFactor(num) {
     // 1. 将数据转换为安全数字
     var n = parseInt(num.toString());
     // 2. 定义factor保存每个质数因子，最小质数为2，保存result记录所有的质数因子集合
-    var factor = 2;
     var result = [];
-    // 3. 如果n > 1时，持续循环找出其余质数因子
-    while (n > 1) {
-        // 4. 如果factor能被n整除，则factor为num的质数因子
-        if (n % factor === 0) {
+    // 3. 先单独处理2，因为2是最小的质数，且是唯一的偶数质数，减少循环次数
+    while (n % 2 === 0) {
+        result.push(2);
+        n = n / 2;
+    }
+    // 4. 从3开始，每次递增2，因为偶数不可能是质数，减少循环次数
+    var factor = 3;
+    while (factor <= Math.sqrt(n)) {
+        // 4.1 如果n能被factor整除，则将factor加入到result中，并将n除以factor
+        while (n % factor === 0) {
             result.push(factor);
-            n = Math.floor(n / factor);
+            n = n / factor;
         }
-        else {
-            // 5. 如果factor无法被n整除，则需要factor++继续寻找
-            factor++;
-        }
+        factor += 2;
+    }
+    // 5. 如果n是一个大于2的质数
+    if (n > 2) {
+        result.push(n);
     }
     return result;
 }
